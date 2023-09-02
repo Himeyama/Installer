7e758f1c5c3f41a9a6b6b923d7f83aa7462bb32f
MURATA Mitsuharu <hikari.photon+dev@gmail.com>
2023-09-03 00:49:08 +0900

    setup.exe の作成

--- build.ps1
+++ build.ps1
@@ -1,3 +1,11 @@
-rm -r -fo bin/
-rm -r -fo obj/
-dotnet build .\Installer.csproj -c Release -r win10-x64
+rm -r -fo PotableInstaller
+rm -r -fo Installer\bin
+rm -r -fo Installer\obj
+dotnet publish .\Installer\Installer.csproj -c Release -r win10-x64
+cp -r Installer/bin/x64/Release/net7.0-windows10.0.19041.0/win10-x64 PotableInstaller
+
+rm -r -fo Setup.exe
+rm -r -fo Setup\bin
+rm -r -fo Setup\obj
+dotnet publish .\Setup\Setup.csproj -c Release -r win-x64
+cp .\Setup\bin\Release\net7.0\win-x64\publish\Setup.exe .
